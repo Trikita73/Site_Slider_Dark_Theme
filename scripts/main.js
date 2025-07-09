1a38947ab6622ce19f99dada0be5af8cf21f382e
Trikita73 <diachenkoandrii7@gmail.com>
2025-07-09 12:34:39 +0300

    header

--- scripts/main.js
+++ scripts/main.js
@@ -1,4 +1,11 @@
 import { themeSwitcher } from '../vendor/theme-switcher/theme-switcher.js'
 new themeSwitcher()
 
+const menuBtn = document.querySelector('.menu-btn')
+const menu = document.querySelector('.main-menu ul')
+
+menuBtn.addEventListener('click', function() {
+    this.classList.toggle('active')
+    menu.classList.toggle('active')
+})
 // document.body.hidden = true // test
