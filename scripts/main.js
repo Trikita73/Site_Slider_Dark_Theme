e37a8a0a8c76c9ea73b20e41d10279e9d3b376f5
Trikita73 <diachenkoandrii7@gmail.com>
2025-07-10 15:27:59 +0300

    swiper

--- scripts/main.js
+++ scripts/main.js
@@ -1,3 +1,4 @@
+import Swiper from '../vendor/swiper/swiper-bundle.min.mjs'
 import { themeSwitcher } from '../vendor/theme-switcher/theme-switcher.js'
 new themeSwitcher()
 
@@ -8,4 +9,17 @@ menuBtn.addEventListener('click', function() {
     this.classList.toggle('active')
     menu.classList.toggle('active')
 })
-// document.body.hidden = true // test
+
+
+const swiper = new Swiper('.head-slider', {
+    effect: 'slide',
+    speed: 1000,
+    loop: true,
+    slidesPerView: 3, 
+    spaceBetween: 30,
+    mousewheel: true,
+    pagination: {
+        el: '.swiper-pagination',
+        clickable: true
+    }
+})
